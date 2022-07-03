e8175be56ec1042ab3a72f35ce13b1ca9da01861
Zane Dax <neptunium0237@gmail.com>
2022-07-03 11:36:56 -0600

    more characters

--- characters.js
+++ characters.js
@@ -1,14 +1,159 @@
 
 let main_characters = {
+    0:{
+        first_name: 'Robert',
+        last_name: 'Ford',
+        pronouns: 'He/Him/His',
+        role: 'Dr. Ford, co-creator of West World park, and head of program designs',
+        status: 'active',
+        interacts_with: 'everyone'
+    },
     1: {
         first_name: 'Dolores',
         last_name: 'Abernathy',
+        pronouns: "She/Her/Hers",
         role: "Rancher's daughter",
         status: "active",
-        interacts_with: ['Father','Man in Black','Teddy Flood']
+        interacts_with: 'scene characters'
+    },
+    2: {
+        first_name: 'Bernards',
+        last_name: 'Lowe',
+        pronouns: 'He/Him/His',
+        role: 'Chief of Operations',
+        status: 'active',
+        interacts_with: 'humans & hosts'
+    },
+    3: {
+        first_name: 'Teddy',
+        last_name: 'Flood',
+        pronouns: 'He/Him/His',
+        role: 'protect Dolores',
+        status: 'active',
+        interacts_with:'scene characters'
+
+    },
+    4:{
+        first_name: 'William',
+        last_name: '',
+        pronouns: 'He/Him/His',
+        role: 'Guest',
+        status: 'active',
+        interacts_with: 'everyone',
+        alias: 'Man in Black' 
+    },
+    5:{
+        first_name: 'Maeve',
+        last_name: 'Millay',
+        pronouns: 'She/Her/Hers',
+        role: 'Manager at Mariposa brothel/ saloon',
+        status: 'active',
+        interacts_with: 'scene characters'
+    },
+    6:{
+        first_name: 'Charlotte',
+        last_name: 'Hale',
+        pronouns: 'She/Her/Hers',
+        role: 'Delco business woman',
+        status: 'active',
+        interacts_with:'humans'
+    },
+    7:{
+        first_name: 'Ashley',
+        last_name: 'Stubbs',
+        pronouns: 'He/Him/His',
+        role: 'Delco park security',
+        status: 'active',
+        interacts_with: 'humans'
+    },
+    8:{
+        first_name: 'Clementine',
+        last_name: 'Pennyfeather',
+        pronouns: 'She/Her/Hers',
+        role: 'sex worker at Mariposa brothel/ saloon',
+        status: 'active',
+        interacts_with: 'scene characters'
+    },
+    9:{
+        first_name: 'Lee',
+        last_name: 'Sizemore',
+        pronouns: 'He/Him/His',
+        role: 'head of designs & narratives',
+        status: 'active',
+        interacts_with:'humans'
+    },
+    10:{
+        first_name: 'Hector',
+        last_name: 'Santoro',
+        pronouns: 'He/Him/His',
+        role: 'outlaw bandit',
+        status: 'active',
+        interacts_with:'scene characters'
+    },
+    11:{
+        first_name: 'Angela',
+        last_name: '',
+        pronouns: 'She/Her/Hers',
+        role: 'West World host to humans',
+        status: 'active',
+        interacts_with:'humans'
+    },
+    12:{
+        first_name: 'Elsie',
+        last_name: 'Hughes',
+        pronouns: 'She/Her/Hers',
+        role: 'Delco Quality Assurance of hosts',
+        status: 'active',
+        interacts_with:'humans & hosts'
+    },
+    13:{
+        first_name: 'Felix',
+        last_name: 'Lutz',
+        pronouns: 'He/Him/His',
+        role: 'Delco host repairman',
+        status: 'active',
+        interacts_with:'hosts'
+    },
+    14:{
+        first_name: 'Sylvester',
+        last_name: '',
+        pronouns: 'He/Him/His',
+        role: '',
+        status: 'Delco host repairman',
+        interacts_with:'hosts'
     }
-    
 
 }
 
-console.log(main_characters[1].role)
+console.log(main_characters[0].first_name)
+
+/*
+
+{
+    first_name: '',
+    last_name: '',
+    pronouns: '',
+    role: '',
+    status: '',
+    interacts_with:''
+}
+
+*/
+
+
+
+
+
+// let Characters = function (fname, lname, role, status, pronouns, alias) {
+//     this.fname = fname
+//     this.lname = lname
+//     this.role = role
+//     this.status = status
+//     this.pronouns = pronouns
+//     this.alias = alias 
+// }
+
+
+
+
+
